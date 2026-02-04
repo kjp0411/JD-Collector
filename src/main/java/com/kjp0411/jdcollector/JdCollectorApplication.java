3106409219bdae260b4895c389e3dd80e9389a88
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2026-02-04 14:57:25 +0900

    chore: initialize Spring Boot project for JD-Collector

---
+++ src/main/java/com/kjp0411/jdcollector/JdCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.kjp0411.jdcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjp0411/jdcollector/JdCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjp0411.jdcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
